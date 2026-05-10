ea4717b3faee5a3a32770a720136619615b226d4
javimotero7777 <javimotero7777@gmail.com>
2026-05-10 19:05:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var SCALENE = 'Scalene triangle';
 function getTriangleType(lengthA, lengthB, lengthC) { 
   var type = '';
   // In Javascript isNaN returns true if the argument is Not a Number (NaN)
-  if ( isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC) ) { //Inputs are not numbers
+  if (isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC)) { // Inputs are not numbers
     type = INVALID_ARGUMENTS;
   } else { // Inputs are numbers
     // Convert from objects to float
@@ -24,12 +24,12 @@ function getTriangleType(lengthA, lengthB, lengthC) {
     var c = parseFloat(lengthC);
     if ((a <= 0) || (b <= 0) || (c <= 0)) { // A side is not positive
       type = INVALID_ARGUMENTS;
-    } else if ( (a+b <= c) || (a+c <= b) || (b+c <= a)) { // invalid triangle
+    } else if ((a + b <= c) || (a + c <= b) || (b + c <= a)) { // Invalid triangle
       type = NOT_A_TRIANGLE;
-    } else { // All sides different = scalene
-      if ( (a==b) && (b==c)) {
+    } else { // Triangle is valid
+      if ((a == b) && (b == c)) {
         type = EQUILATERAL;
-      } else if ( (a==b) || (b==c) || (a==c)) {
+      } else if ((a == b) || (b == c) || (a == c)) {
         type = ISOSCELES;
       } else {
         type = SCALENE;
@@ -37,6 +37,12 @@ function getTriangleType(lengthA, lengthB, lengthC) {
     }
   }
 
-  
+  if (typeof gtag !== 'undefined') {
+    gtag('event', 'triangle_calculated', {
+      'triangle_type': type,
+      'timestamp': new Date().toISOString()
+    });
+  }
+
   return type;
 }
